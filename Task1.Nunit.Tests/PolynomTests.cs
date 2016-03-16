31a253124ab33235c77c7705711439b7e4f45928
Olga Lozovik <lozovikolga@gmail.com>
2016-03-17 02:52:11 +0300

    Tasks of the third day

---
+++ Task1.Nunit.Tests/PolynomTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace Task1.Nunit.Tests
+{
+    [TestFixture]
+    public class PolynomTests
+    {
+        [TestCase(new double[] { 0, 1, 10 }, Result = "x^1 + 10*x^2")]
+        [TestCase(new double[] { 0, 0, 10 }, Result = "10*x^2")]
+        [TestCase(new double[] { 5, 1, 10 }, Result = "5*x^0 + x^1 + 10*x^2")]
+        [TestCase(new double[] { -8, 1, -10 }, Result = "-8*x^0 + x^1 - 10*x^2")]
+        [TestCase(new double[] { 0 },  Result = "")]
+        public string ToString_Test(double[] coefficients)
+        {
+            return new Polynom(coefficients).ToString();
+        }
+
+        [TestCase(new double[] {}, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, ExpectedException = typeof(ArgumentNullException))]
+        [TestCase(new double[] { 0, 1, 10 }, Result = "x^1 + 10*x^2")]
+        public string Constructor_Test(double[] coefficients)
+        {
+            return new Polynom(coefficients).ToString();
+        }
+
+        #region Arithmetic functions Tests
+
+        [TestCase(new double[] {0, 1, 10, -5 }, new double[] {0, 0, 2, 4 }, Result = "x^1 + 12*x^2 - x^3")]
+        [TestCase(new double[] { 0, 1, 10 }, new double[] { 4 }, Result = "4*x^0 + x^1 + 10*x^2")]
+        [TestCase(new double[] { 4 }, new double[] { 0, 1, 10 }, Result = "4*x^0 + x^1 + 10*x^2")]
+        [TestCase(new double[] { 0 }, new double[] { 0 }, Result = "")]
+        [TestCase(new double[] { 0, 1, 10 }, new double[] { 0, 0, 2, 4 }, Result = "x^1 + 12*x^2 + 4*x^3")]
+        [TestCase(new double[] { 0 }, new double[] { 5, 0, -1, -4 }, Result = "5*x^0 - x^2 - 4*x^3")]
+        public string Add_Test(double[] lhs, double[] rhs)
+        {
+            return (new Polynom(lhs) + new Polynom(rhs)).ToString();
+        }
+
+        [TestCase(new double[] { 0, 1, 10, -5 }, new double[] { 0, 0, 2, 4 }, Result = "x^1 + 8*x^2 - 9*x^3")]
+        [TestCase(new double[] { -5, -1.5, 10 }, new double[] { -4 }, Result = "-x^0 - 1.5*x^1 + 10*x^2")]
+        [TestCase(new double[] { 0, 1, 10 }, new double[] { 4 }, Result = "-4*x^0 + x^1 + 10*x^2")]
+        [TestCase(new double[] { 0, 1, 10 }, new double[] { 0, 0, 2, 4 }, Result = "x^1 + 8*x^2 - 4*x^3")]
+        [TestCase(new double[] { 0 }, new double[] { 0, 0, 2, 4 }, Result = "-2*x^2 - 4*x^3")]
+        public string Substruct_Test(double[] lhs, double[] rhs)
+        {
+            return (new Polynom(lhs) - new Polynom(rhs)).ToString();
+        }
+
+        [TestCase(new double[] { 0, 1 }, new double[] { 1, 1, 1, 1 }, Result = "x^1 + x^2 + x^3 + x^4")]
+        [TestCase(new double[] { 1, 1, 1, 1 }, new double[] { 0, 1 }, Result = "x^1 + x^2 + x^3 + x^4")]
+        [TestCase(new double[] { 1, 1 }, new double[] { 1, 1 }, Result = "x^0 + 2*x^1 + x^2")]
+        [TestCase(new double[] { -1, 1 }, new double[] { 1, 1 }, Result = "-x^0 + x^2")]
+        [TestCase(new double[] { -1, -1 }, new double[] { -1, -1 }, Result = "x^0 + 2*x^1 + x^2")]
+        [TestCase(new double[] { 15, -1 }, new double[] { -10, -1 }, Result = "-150*x^0 - 5*x^1 + x^2")]
+        [TestCase(new double[] { 0, 0, 1, 0, 1 }, new double[] { 0, 1, 0, 1 }, Result = "x^3 + 2*x^5 + x^7")]
+        [TestCase(new double[] { 0 }, new double[] { 0, 0, 2, 4 }, Result = "")]
+        public string Multiply_Test(double[] lhs, double[] rhs)
+        {
+            return (new Polynom(lhs) * new Polynom(rhs)).ToString();
+        }
+        #endregion
+
+        #region GetHashCode Tests
+
+        [TestCase(new double[] { 0, 1 }, new double[] { 0, 1, 1, 1 }, Result = false)]
+        [TestCase(new double[] { 0, 1 }, new double[] { 0, 1, 0, 0 }, Result = true)]
+        [TestCase(new double[] { 0, 1 }, new double[] { 0, 1 }, Result = true)]
+        [TestCase(new double[] { 0, 1 }, new double[] { 0, 0 }, Result = false)]
+        public bool GetHashCode_DifferentObjects(double[] coefficients1, double[] coefficients2)
+        {
+            int hash1 = new Polynom(coefficients1).GetHashCode();
+            int hash2 = new Polynom(coefficients2).GetHashCode();
+            Debug.WriteLine(hash1);
+            Debug.WriteLine(hash2);
+            return  hash1 == hash2;
+        }
+
+        [TestCase(new double[] { 0, 1 }, Result = true)]
+        public bool GetHashCode_TheSameObject(double[] coefficients)
+        {
+            Polynom polynom = new Polynom(coefficients);
+            int hash1 = polynom.GetHashCode();
+            int hash2 = polynom.GetHashCode();
+            return hash1 == hash2;
+        }
+        #endregion
+
+        #region Equals Tests
+        [TestCase(new double[] { 0, 1 }, Result = true)]
+        public bool Equals_TheSameObject(double[] coefficients)
+        {
+            Polynom polynom = new Polynom(coefficients);
+            return polynom.Equals(polynom);
+        }
+
+        [TestCase(new double[] { 0, 1 }, new double[] { 0, 1, 1, 1 }, Result = false)]
+        [TestCase(new double[] { 0, 1 }, new double[] { 0, 1, 0, 0 }, Result = true)]
+        [TestCase(new double[] { 0, 1 }, new double[] { 0, 1 }, Result = true)]
+        [TestCase(new double[] { 0, 1 }, new double[] { 0, 0 }, Result = false)]
+        public bool Equals_DifferentObjects(double[] coefficients1, double[] coefficients2)
+        {
+            Polynom polynom = new Polynom(coefficients1);
+            return polynom.Equals(new Polynom(coefficients2));
+        }
+
+        [TestCase(new double[] { 0, 1 }, Result = false)]
+        public bool Equals_WithNull(double[] coefficients)
+        {
+             Polynom polynom = new Polynom(coefficients);
+             return polynom.Equals(null);
+        }
+
+        [TestCase(new double[] { 0, 1 }, Result = false)]
+        [TestCase(5, Result = false)]
+        public bool Equals_WithObjectParametr(object parametr)
+        {
+            Polynom polynom = new Polynom(1, 2, 3, 4);
+            return polynom.Equals(null);
+        }
+        #endregion
+
+    }
+}
---
+++ Task1/Polynom.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Task1
+{
+    public class Polynom : IEquatable<Polynom>
+    {
+        private readonly double[] coefficients;
+
+        public Polynom(params double[] coefficients)
+        {
+            if (coefficients == null)
+            {
+                throw new ArgumentNullException(nameof(coefficients));
+            }
+
+            if (coefficients.Length == 0)
+            {
+                throw new ArgumentException("Coefficient array can't be empty!");
+            }
+
+            this.coefficients = new double[LastCoefficientIndex(coefficients) + 1];
+            for (int i = 0; i < this.coefficients.Length; i++)
+            {
+                this.coefficients[i] = coefficients[i];
+            }
+        }
+
+        #region Override System.Object methods
+        public override string ToString()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            string result = "";
+            bool isFirstElement = true;
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                if (coefficients[i].Equals(0)) continue;
+
+                if (isFirstElement)
+                {
+                    isFirstElement = false;
+
+                    if (coefficients[i] < 0)
+                    {
+                        result += GetSign(coefficients[i]);
+                    }
+
+                    if (Math.Abs(coefficients[i]).Equals(1))
+                    {
+                        result += $"x^{i}";
+                    }
+                    else
+                    {
+                        result += $"{Math.Abs(coefficients[i])}*x^{i}";
+                    }                       
+                }
+                else
+                {
+                    if (Math.Abs(coefficients[i]).Equals(1))
+                    {
+                        result += $" {GetSign(coefficients[i])} x^{i}";
+                    }
+                    else
+                    {
+                        result += $" {GetSign(coefficients[i])} {Math.Abs(coefficients[i])}*x^{i}";
+                    }                      
+                }
+            }
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 12345;
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                hash += hash ^ coefficients[i].GetHashCode();
+            }
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Polynom polynom = obj as Polynom;
+            return Equals(polynom);
+        }
+        #endregion
+
+        #region  Implementation of the interface IEquatable<T>
+        public bool Equals(Polynom polynom)
+        {
+            if ((object)polynom == null)
+            {
+                return false;
+            }
+
+            return CompareArrays(coefficients, polynom.coefficients);
+        }
+        #endregion
+
+        #region Overloaded operators
+        public static bool operator ==(Polynom lhs, Polynom rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+            {
+                return true;
+            }
+
+            if ((object)lhs != null)
+            {
+                return lhs.Equals(rhs);
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Polynom lhs, Polynom rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public static Polynom operator +(Polynom lhs, Polynom rhs)
+        {
+            return Add(lhs, rhs);
+        }
+
+        public static Polynom operator -(Polynom lhs, Polynom rhs)
+        {
+            return Substract(lhs, rhs);
+        }
+
+        public static Polynom operator *(Polynom lhs, Polynom rhs)
+        {
+            return Multiply(lhs, rhs);
+        }
+        #endregion
+
+        #region Arithmetic functions
+        public static Polynom Add(Polynom lhs, Polynom rhs)
+        {
+            int maxLength = Max(lhs.coefficients.Length, rhs.coefficients.Length);
+            double[] newCoefficients = new double[maxLength];
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < lhs.coefficients.Length)
+                {
+                    newCoefficients[i] += lhs.coefficients[i];
+                }
+                if (i < rhs.coefficients.Length)
+                {
+                    newCoefficients[i] += rhs.coefficients[i];
+                }
+            }
+            return new Polynom(newCoefficients);
+        }
+
+        public static Polynom Substract (Polynom lhs, Polynom rhs)
+        {
+            int maxLength = Max(lhs.coefficients.Length, rhs.coefficients.Length); 
+            double[] newCoefficients = new double[maxLength];
+           
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < lhs.coefficients.Length)
+                {
+                    newCoefficients[i] += lhs.coefficients[i];
+                }
+                if (i < rhs.coefficients.Length)
+                {
+                    newCoefficients[i] -= rhs.coefficients[i];
+                }
+            }
+
+            return new Polynom(newCoefficients);
+        }
+
+        public static Polynom Multiply(Polynom lhs, Polynom rhs)
+        {
+            double[] tempCoefficients = new double[lhs.coefficients.Length * rhs.coefficients.Length];
+
+            for (int i = 0; i < lhs.coefficients.Length; i++)
+            {
+                for (int j = 0; j < rhs.coefficients.Length; j++)
+                {
+                    tempCoefficients[i + j] += lhs.coefficients[i] * rhs.coefficients[j];
+                }
+            }
+
+            double[] newCoefficients = new double[LastCoefficientIndex(tempCoefficients) + 1];
+            for (int i = 0; i < newCoefficients.Length; i++)
+            {
+                newCoefficients[i] = tempCoefficients[i];
+            }
+
+            return new Polynom(newCoefficients);
+        }
+        #endregion
+
+        #region Private methods
+        private static int Max(int a, int b) => a < b ? b : a;
+
+        private static int LastCoefficientIndex(double[] array)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (!array[i].Equals(0))
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        private string GetSign(double value) => value >= 0 ? "+" : "-";
+
+        private bool CompareArrays(double[] array1, double[] array2 )
+        {
+            if (array1.Length != array2.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (!Equals(array1[i], array2[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+#endregion
+
+    }
+}
